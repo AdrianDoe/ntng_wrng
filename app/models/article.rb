d9a0cba08bf5843d9d9427ebcb4ecb21902dc5af
AdrianDoe <adrian.adoe@gmail.com>
2017-12-07 17:30:36 +0200

    admin article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  def news_view
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :articles
+
   root 'frontpage#index'
   get 'frontpage/index'
+
+  resources :articles, only: [:show, :index]
+
+  namespace :admin do
+    resources :articles
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
